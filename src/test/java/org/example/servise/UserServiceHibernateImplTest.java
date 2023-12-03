52c82b6a32d9d52c6ad3a9be57ce7600c656c223
Vetal <heavyve@gmail.com>
2023-12-03 23:47:17 +0300

    Add tests

---
+++ src/test/java/org/example/servise/UserServiceHibernateImplTest.java
@@ -0,0 +1,32 @@
+package org.example.servise;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class UserServiceHibernateImplTest {
+
+    @Test
+    void createUserTable() {
+    }
+
+    @Test
+    void dropUserTable() {
+    }
+
+    @Test
+    void saveUser() {
+    }
+
+    @Test
+    void removeUserById() {
+    }
+
+    @Test
+    void getAllUsers() {
+    }
+
+    @Test
+    void clearUserTable() {
+    }
+}
