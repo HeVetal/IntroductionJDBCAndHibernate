855c7da0b7a8813800306752280362c4cc33ec40
Vetal <heavyve@gmail.com>
2023-12-13 23:08:57 +0300

    Add getHibernateConnection, getSessionFactoryProperties, getSessionFactory resources add hibernate.cfg.xml, hibernate.properties

--- src/main/java/org/example/util/Util.java
+++ src/main/java/org/example/util/Util.java
@@ -3,12 +3,20 @@ package org.example.util;
 import com.mysql.cj.Session;
 import lombok.AccessLevel;
 import lombok.NoArgsConstructor;
+import org.example.model.User;
+import org.hibernate.HibernateException;
 import org.hibernate.SessionFactory;
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
+import org.hibernate.cfg.Configuration;
+import org.hibernate.cfg.Environment;
+import org.hibernate.service.ServiceRegistry;
+
 
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 import java.util.Objects;
+import java.util.Properties;
 
 @NoArgsConstructor(access = AccessLevel.PRIVATE)
 public class Util {
@@ -29,11 +37,61 @@ public class Util {
         return connection;
     }
 
-    public static SessionFactory getHibernateConnection(){
-        if(Objects.isNull(sessionFactory)){
+    public static SessionFactory getHibernateConnection() {
+        if (Objects.isNull(sessionFactory)) {
+            try {
+                Configuration configuration = new Configuration();
+                Properties properties = new Properties();
+                properties.put(Environment.DRIVER, "com.mysql.cj.jdbc.Driver");
+                properties.put(Environment.URL, URL);
+                properties.put(Environment.USER, USER_NAME);
+                properties.put(Environment.PASS, PASSWORD);
+                properties.put(Environment.DIALECT, "org.hibernate.dialect.MySQL5Dialect");
+                properties.put(Environment.SHOW_SQL, "true");
+                properties.put(Environment.CURRENT_SESSION_CONTEXT_CLASS, "thread");
+                properties.put(Environment.HBM2DDL_AUTO, "validate");
+                configuration.setProperties(properties);
+                configuration.addAnnotatedClass(User.class);
 
+                ServiceRegistry serviceRegistry = new StandardServiceRegistryBuilder()
+                        .applySettings(configuration.getProperties()).build();
+
+                sessionFactory = configuration.buildSessionFactory(serviceRegistry);
+            } catch (HibernateException e) {
+                throw new RuntimeException(e);
+            }
         }
         return sessionFactory;
     }
+
+    public static SessionFactory getSessionFactoryProperties(){
+        if(Objects.isNull(sessionFactory)){
+            try {
+                Configuration configuration = new Configuration();
+                configuration.addAnnotatedClass(User.class);
+
+                StandardServiceRegistryBuilder serviceRegistry = new StandardServiceRegistryBuilder()
+                        .applySettings(configuration.getProperties());
+
+                sessionFactory = configuration.buildSessionFactory(serviceRegistry.build());
+            } catch (HibernateException e) {
+                throw new RuntimeException(e);
+            }
+        }
+        return sessionFactory;
+    }
+
+    public static SessionFactory getSessionFactory(){
+        if(Objects.isNull(sessionFactory)){
+            try {
+                Configuration configuration = new Configuration().configure();
+                sessionFactory = configuration.buildSessionFactory();
+            } catch (HibernateException e){
+                throw new RuntimeException(e);
+            }
+        }
+        return sessionFactory;
+    }
+
 
 }
